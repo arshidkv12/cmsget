9296a72acf8ec306aae764dc2afab8a8c6ecc386
Arshid <arshidkv12@gmail.com>
2017-01-27 01:39:34 +0530

    Update cmsget.sh

--- cmsget.sh
+++ cmsget.sh
@@ -29,5 +29,57 @@ apt-get install -y postfix
 service apache2 restart
 /etc/init.d/mysql restart
 
-echo $PASSWORD
+mysql -uroot -p$PASSWORD <<MYSQL_SCRIPT
+CREATE DATABASE $1;
+CREATE USER '$1'@'localhost' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+echo "MySQL user created."
+echo "Username:   $1"
+ 
+
+echo "============================================"
+echo "A robot is now installing WordPress for you."
+echo "============================================"
+#download wordpress
+wget https://wordpress.org/latest.tar.gz
+#unzip wordpress
+tar -zxvf latest.tar.gz
+#change dir to wordpress
+cd wordpress
+#copy file to parent dir
+cp -rf . ..
+#move back to parent dir
+cd ..
+#remove files from wordpress folder
+rm -R wordpress
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$1/g" wp-config.php
+perl -pi -e "s/username_here/$1/g" wp-config.php
+perl -pi -e "s/password_here/$PASSWORD/g" wp-config.php
+
+#set WP salts
+perl -i -pe'
+  BEGIN {
+    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+  }
+  s/put your unique phrase here/salt()/ge
+' wp-config.php
+
+#create uploads folder and set permissions
+mkdir wp-content/uploads
+chmod 775 wp-content/uploads
+echo "Cleaning..."
+#remove zip file
+rm latest.tar.gz
+#remove bash script
+echo "========================="
+echo "Installation is complete."
+echo "========================="
 
