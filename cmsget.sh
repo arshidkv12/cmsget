30266db2adc79954ed1e49f83dc233221a3d1a44
Arshid <arshidkv12@gmail.com>
2017-01-26 23:42:48 +0530

    Update cmsget.sh

--- cmsget.sh
+++ cmsget.sh
@@ -7,6 +7,12 @@ apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-commo
 apt-get install php5-curl php5-dbg  php5-xmlrpc php5-fpm php-apc php-pear php5-imap -y
 apt-get install php5-pspell php5-dev -y 
 
+#mail
+apt-get install php-pear 
+pear install mail 
+pear install Net_SMTP 
+pear install Auth_SASL 
+pear install mail_mime
 
 
 
