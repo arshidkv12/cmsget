e811807cbb1f3ba77222d123780cc1d6f1b95993
arshidkv12 <arshidkv12@gmail.com>
2017-01-26 10:50:41 +0530

    cmsget install comments

---
+++ cmsget.sh
@@ -0,0 +1,8 @@
+apt-get update -y
+apt-get install apache2 -y 
+apt-get install mysql-server php5-mysql -y
+mysql_install_db -y
+mysql_secure_installation -y
+apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-common -y
+apt-get install php5-curl php5-dbg  php5-xmlrpc php5-fpm php-apc php-pear php5-imap -y
+apt-get install php5-pspell php5-dev -y 
